bf8b3cc5556734c38f4b0a17fcd97ba7802b1e7b
이준원 <ljw1205317@naver.com>
2026-04-01 16:22:42 +0900

    브랜치 추가

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </div>
         <div>
           <h1>vite + React</h1>
+          <h2>Login Feature</h2>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
